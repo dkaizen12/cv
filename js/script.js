58cb7d2cd80323fd6ef35647a0f385ad03138cbe
Dady <dadykalangoso@gmail.com>
2026-04-14 22:11:52 +0200

    style : quelques animations css simples

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // 1. Menu Burger (Dynamisme de navigation)
-    const nav = document.querySelector('nav ul');
-    const header = document.querySelector('header');
-    
-    const burgerBtn = document.createElement('button');
-    burgerBtn.innerHTML = '<i class="fas fa-bars"></i>';
-    burgerBtn.className = 'burger-menu';
-    header.prepend(burgerBtn);
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      document.querySelector(".charge").classList.add("cache");
+    }, 3600); // 3 secondes
+  });
+  // 1. Menu Burger (Dynamisme de navigation)
+  //const nav = document.querySelector("nav ul");
+ // const header = document.querySelector("header");
 
-    burgerBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
+  //const burgerBtn = document.createElement("button");
+  //burgerBtn.innerHTML = '<i class="fas fa-bars"></i>';
+  //burgerBtn.className = "burger-menu";
+  //header.prepend(burgerBtn);
 
-    // 2. Contrôle de saisie formulaire [cite: 38]
-    const contactForm = document.querySelector('form');
-    
-    contactForm.addEventListener('submit', (e) => {
-        const message = document.getElementById('message').value;
-        
-        if (message.length < 10) {
-            e.preventDefault();
-            alert("Votre message doit contenir au moins 10 caractères.");
-        } else {
-            alert("Merci ! Votre message a été envoyé (Simulation).");
-        }
-    });
+ // burgerBtn.addEventListener("click", () => {
+    //nav.classList.toggle("active");
+  //});
 
-    // 3. Animation fluide pour les ancres [cite: 13]
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+  // 2. Contrôle de saisie formulaire [cite: 38]
+  const contactForm = document.querySelector("form");
+
+  contactForm.addEventListener("submit", (e) => {
+    const message = document.getElementById("message").value;
+
+    if (message.length < 10) {
+      e.preventDefault();
+      alert("Votre message doit contenir au moins 10 caractères.");
+    } else {
+      alert("Merci ! Votre message a été envoyé (Simulation).");
+    }
+  });
+
+  // 3. Animation fluide pour les ancres [cite: 13]
+  document.querySelectorAll("nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 });
